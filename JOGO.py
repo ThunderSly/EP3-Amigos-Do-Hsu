6412d6b07e318533036f3e46732bbf231b0831cd
Vinicius Hsu <vinihsu1997@hotmail.com>
2017-05-04 15:15:26 -0300

    comentários gerais no programa

--- JOGO.py
+++ JOGO.py
@@ -1,8 +1,16 @@
+#   ===================================================================================================================================================================================================================================
+#   =================================================================================================   HSU POKER   ===================================================================================================================
+#   ===================================================================================================================================================================================================================================
+
+
+#   =====================================================   Imports   ============================================================
+
 import time
 import pickle
-lista_arquivos=[]
 
-def salvar(arquivo,nome, dinheiro,xp):
+#   =====================================================   Funções   ============================================================
+
+def salvar(arquivo,nome, dinheiro,xp):  # Função de salva o jogo
 
 	lista_arquivos.append(arquivo)
 	saves=open("lista", "wb" )
@@ -14,23 +22,29 @@ def salvar(arquivo,nome, dinheiro,xp):
 	time.sleep(0.5)
 	print("Jogo salvo com sucesso!")
 
+#   =====================================================   Listas   ==============================================================
 
-sim=["sim", "s"]
-nao=["nao","n","não"]
+sim=["sim", "s"]  # Lista para inputs afirmativos
+nao=["nao","n","não"]  # Lista para inputs negativos
+lista_arquivos=[]  # Lista de jogos salvos
 
-print("Bem vindo ao Hsu Poker ")
+#   =====================================================   Início   ==============================================================
 
-abrir_salvo=input("Deseja abrir um jogo salvo? \n")  #Verifica se o usário quer abrir um jogo salvo
+print("Bem vindo ao Hsu Poker ") # Começo do jogo
+
+#   =============================================   Abertura de jogo salvo  =======================================================
+
+abrir_salvo=input("Deseja abrir um jogo salvo? \n")  # Verifica se o usário quer abrir um jogo salvo
 time.sleep(0.5)
 
 while True:
-
-	if abrir_salvo in sim:
+	
+	if abrir_salvo in sim: # Caso o usuário queira abri um jogo
 
 		try:
-			lista_arquivos=pickle.load(open("lista","rb"))
+			lista_arquivos=pickle.load(open("lista","rb"))  # Verifica a existência de jogos salvos
 
-			if len(lista_arquivos)>0:
+			if len(lista_arquivos)>0: # Mostra os arquivos salvos existentes
 				print("Os saves disponíveis são:")
 				time.sleep(0.5)
 
@@ -38,7 +52,7 @@ while True:
 					print("{}".format(i))
 					time.sleep(0.5)
 
-				arquivo=str(input("Qual save deseja carregar? "))
+				arquivo=str(input("Qual save deseja carregar? "))  # Usuário escolhe um jogo pra abrir
 				dado=pickle.load(open(arquivo,"rb"))  # Traz o jogo salvo com os dados guardados
 				nome=dado["{}".format(arquivo)][0]
 				dinheiro=dado["{}".format(arquivo)][1]
@@ -46,7 +60,7 @@ while True:
 				carregado=sim
 				print("Carregando dados...")
 				time.sleep(2)
-				print("Você possui {} de dinheiro e {} de experiência" .format(dinheiro,xp))
+				print("Você possui {} de dinheiro e {} de experiência" .format(dinheiro,xp)) # Caracterização dos dados para o usuário
 				break
 
 
@@ -64,16 +78,20 @@ while True:
 		print("Digite um comando válido: sim ou não")
 		continue
 
+#   ==========================================   Caracterização do personagem  ===================================================
+
 while True:
-	if carregado==sim: # Checa se houve o carregamento de dados para pular o processo de escolha de um inspermon
+	if carregado==sim: # Checa se houve o carregamento de dados para pular o processo de caracterizar um personagem novo
 			break
 		
 	if carregado==nao:  # Caso não tenha nenhum arquivo salvo aberto
 		nome=input("Qual vai ser o nome do seu personagem?\n")
 		dinheiro= 10000
 		xp=0
-		print(" Você tem 10 mil fichas para iniciar sua trajetória")
+		print(" Você tem 10 mil fichas para iniciar sua trajetória") # Definições iniciais
 		break
+
+#   =============================================   Salvamento do progresso  ======================================================
 
 while True:
 
@@ -92,6 +110,8 @@ while True:
 		else:
 			print("Digite um comando válido (Sim ou Não)")
 			continue
-			
+	
+#   ===================================================   Fim do jogo  =============================================================
+
 time.sleep(0.5)
-print("Até a próxima!")
+print("Até a próxima!") # Finalização do jogo
