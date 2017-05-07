9b42677e2e6059f2247e1f51d81bc34b1d083225
Bruno Costa <brunex887@gmail.com>
2017-05-07 18:57:35 -0300

    Add files via upload

--- Classes_deck.py
+++ Classes_deck.py
@@ -55,10 +55,11 @@ class Deck:
 
 class Jogador:
 
-	def __init__(self, nome):
+	def __init__(self, nome,fichas):
 
 		self.nome = nome
 		self.mao = []
+		self.fichas = fichas
 		lista_jogadores.append(self)
 
 	def compra_carta(self, deck):
@@ -72,18 +73,74 @@ class Jogador:
 		for carta in self.mao:
 			carta.show()
 
+	def acao(self,deck):
+		if maior_aposta == 0: 
+			acao=input("Check(C), Raise(R), Fold(F)").lower()
+			if acao == "check" or acao == "c":
+				print("{} checa!".format(self))
+			if acao == "raise" or acao == "r":
+				aposta=int(input("Quanto deseja apostar?"))
+				self.fichas -=aposta
+				print("{} aposta {} fichas!".format(self,aposta))
+				if aposta > maior_aposta:
+					maior_aposta = aposta
+				pot+=aposta
+			if acao == "fold" or acao == "f":
+				lista_jogadores.remove(self)
+				print("{} sai da rodada!".format(self))
+
+		if maior_aposta > 0:
+			acao=input("Call(C), Raise(R), Fold(F)").lower()
+			if acao == "call" or acao == "c":
+				print("{} paga pra ver!".format(self))
+				self.fichas -= maior_aposta
+				pot+=aposta
+			if acao == "raise" or acao == "r":
+				aposta=int(input("Quanto deseja apostar?"))
+				self.fichas -=aposta
+				print("{} aposta {} fichas!".format(self,aposta))
+				if aposta > maior_aposta:
+					maior_aposta = aposta
+				pot+=aposta
+			if acao == "fold" or acao == "f":
+				lista_jogadores.remove(self)
+				print("{} sai da rodada!".format(self))
+
+
 class Rodada:
 
 	def __init__(self,lista_jogadores):
 		lista_jogadores=[]
+		pot=0
 		deck = Deck()
 		deck.shuffle()
 		for i in lista_jogadores:
 			i.compra_carta(deck).compra_carta(deck)
 			i.mostra_mao()
 
-
-
+	def flop(self,deck):
+		maior_aposta=0
+		mesa=[]
+		mesa.append(deck.compra())
+		mesa.append(deck.compra())
+		mesa.append(deck.compra())
+		print(mesa[0]+ " - " + mesa[1] + " - " + mesa[2])
+		for i in lista_jogadores:
+			i.acao(deck)
+	
+	def turn(self,deck):
+		maior_aposta=0
+		mesa.append(deck.compra())
+		print(mesa[0] + " - " + mesa[1] + " - " + mesa[2] + " - " + mesa[3])
+		for i in lista_jogadores:
+			i.acao(deck)
+	
+	def river(self,deck):
+		maior_aposta=0
+		mesa.append(deck.compra())
+		print(mesa[0] + " - " + mesa[1] + " - " + mesa[2] + " - " + mesa[3] + " - " + mesa[4])
+		for i in lista_jogadores:
+			i.acao(deck)
 
 
 
