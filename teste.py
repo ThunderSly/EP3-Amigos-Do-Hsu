57491bb9e408be20b36609ac59f17e69bb29bd86
Vinicius Hsu <vinihsu1997@hotmail.com>
2017-05-16 17:13:01 -0300

    funcionalidade incompleta

--- teste.py
+++
@@ -1,9 +0,0 @@
-import pickle
-dado = pickle.load(open("jogo",'wb'))  
-nome = dado["jogo"][0]
-fichas = dado["jogo"][1]
-xp = dado["jogo"][2]
-carregado = sim
-jogador = Jogador(nome, fichas, xp)
-time.sleep(2)
-print("O nome do seu personagem é {}, você possui {} de fichas e {} de experiência" .format(nome, fichas, xp)) # Caracterização dos dados para o usuário
