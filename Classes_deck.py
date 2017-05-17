2a4c0a9bb5b59c71fd1171283e434bb03189241d
Bruno Costa <brunex887@gmail.com>
2017-05-17 16:25:24 -0300

    Raise certo

--- Classes_deck.py
+++ Classes_deck.py
@@ -91,12 +91,20 @@ class Jogador: # Jogador
 
 				if acao == "raise" or acao == "r": # Aposta: coloca uma aposta na mesa
 					aposta=int(input("Quanto deseja apostar?\n"))
-					self.fichas -=aposta
-					print("{} aposta {} fichas!".format(self,aposta))
-
-					if aposta > maior_aposta:
-						maior_aposta = aposta
-					pot+=aposta
+					if aposta<self.fichas:
+						self.fichas -=aposta
+						print("{} aposta {} fichas!".format(self,aposta))
+						if aposta > maior_aposta:
+							maior_aposta = aposta
+						pot+=aposta
+					if aposta == self.fichas:
+						self.fichas == 0
+						print("{} ESTA ALL IN!".format(self))
+						if aposta > maior_aposta:
+							maior_aposta = aposta
+						pot+=aposta
+					if aposta > self.fichas:
+						print("Você não tem essa quantidade de fichas!")
 
 				if acao == "fold" or acao == "f": # Fold: desiste da mão
 					lista_jogadores.remove(self)
@@ -110,14 +118,22 @@ class Jogador: # Jogador
 					self.fichas -= maior_aposta
 					pot+=aposta
 
-				if acao == "raise" or acao == "r": # Aumenta a aposta
+					if acao == "raise" or acao == "r": # Aposta: coloca uma aposta na mesa
 					aposta=int(input("Quanto deseja apostar?\n"))
-					self.fichas -=aposta
-					print("{} aposta {} fichas!".format(self,aposta))
-
-					if aposta > maior_aposta:
-						maior_aposta = aposta
-					pot+=aposta
+					if aposta<self.fichas:
+						self.fichas -=aposta
+						print("{} aposta {} fichas!".format(self,aposta))
+						if aposta > maior_aposta:
+							maior_aposta = aposta
+						pot+=aposta
+					if aposta == self.fichas:
+						self.fichas == 0
+						print("{} ESTA ALL IN!".format(self))
+						if aposta > maior_aposta:
+							maior_aposta = aposta
+						pot+=aposta
+					if aposta > self.fichas:
+						print("Você não tem essa quantidade de fichas!")
 
 				if acao == "fold" or acao == "f": # Fold: desiste da rodada
 					lista_jogadores.remove(self)
@@ -268,114 +284,6 @@ class Rodada: # Rodada
 		for i in lista_jogadores: # Confere a ação de cada jogador na rodada
 			i.acao(deck)
 		return mesa
-
-	def melhor_mao():	
-		
-		cartas_jogadores = mesa + mao
-		lista_valores = []
-		lista_naipes = []
-		combinacoes = []
-		valor_mao = 0
-
-		for i in cartas_jogadores:
-			lista_valores.append(i.valor)
-			lista_naipes.append(i.naipe)
-			if i.valor == 1:
-				lista_valores.replace(i.valor, 14) # valores das cartas
-			
-		for i in lista_valores: #para definir pares,trincas e quadras
-							
-			if lista_valores.count(i) == 4: # Quadra
-				for x in range(2):
-					combinacoes.append(i)
-							
-			elif lista_valores.count(i) == 3:
-				combinacoes.append(i)
-
-			elif lista_valores.count(i) == 2:
-				combinacoes.append(i)
-
-		combinacoes.sort()
-
-		if combinacoes[0] == combinacoes[1] and combinacoes[2] == combinacoes[3] and combinacoes[4] == combinacoes[5]: #retirar o terceiro par
-			if combinacoes[0] != combinacoes[2]:
-				del combinacoes[0]
-				del combinacoes[0]
-
-		if len(combinacoes) == 7:
-			maior = []
-			for i in lista_valores:
-				if lista_valores.count(i) == 2:
-					maior.append(i)
-			maior.sort()
-			combinacoes.remove(maior[0])
-			combinacoes.remove(maior[0])
-
-		if len(combinacoes) == 2: # Par
-			print("Par de {}".format(i))
-			valor_mao = 2			
-			
-		if len(combinacoes) == 4: # Dois Pares
-			print("Dois pares de {} e de {}".format(combinacoes[0] , combinacoes[2]))
-			valor_mao = 3
-			
-		if len(combinacoes) == 3: # Trinca
-			print("Trinca de {}".format(i))
-			valor_mao = 4
-
-		for i in lista_valores:
-			if i.valor == 14:
-				lista_valores.append(1)
-				lista_naipes.append(i.naipe)		
-		
-		for n in range(1, 11):				
-			if n in lista_valores: #Straight
-				if n+1 in lista_valores:
-					if n+2 in lista_valores:
-						if n+3 in lista_valores:
-							if n+4 in lista_valores:
-								print("Straight de {} a {}".format(n,n+4))
-								valor_mao = 5
-		for i in lista_valores:
-			if i.valor == 1:
-				lista_valores.remove(i.valor)
-				lista_naipes.remove(i.naipe)
-
-		for i in lista_naipes: 
-			if lista_naipes.count(i) >= 5: #Flush
-				print("Flush de {}".format(i))
-				valor_mao = 6
-			
-		if len(combinacoes) == 5 or len(combinacoes) == 6: # Full House
-			print("Full House")
-			valor_mao = 7
-			
-		if len(combinacoes) >= 7: #Quadra
-			valor_mao = 8
-
-		for i in lista_valores:
-			if i.valor == 14:
-				lista_valores.append(1)
-				lista_naipes.append(i.naipe)
-		
-		for n in range(1,11): # Straight Flush				
-			for c in ["Ouros","Paus","Copas","Espadas"]:
-				for i in cartas_jogadores:
-					if i.n in lista_valores and i.c in lista_naipes:
-						if i.n + 1 in lista_valores and i.c in lista_naipes:
-							if i.n + 2 in lista_valores and i.c in lista_naipes:
-								if i.n + 3 in lista_valores and i.c in lista_naipes:
-									if i.n + 4 in lista_valores and i.c in lista_naipes:
-										valor_mao = 9
-										if i.n == 10: #Royal Straight Flush
-											valor_mao = 10
-		
-		for i in lista_valores:
-			if i.valor == 1:
-				lista_valores.remove(i.valor)
-				lista_naipes.remove(i.naipe)
-
-		return valor_mao
 
 	def Maos_iguais(self, valor_mao):
 		valor_especifico = 0
